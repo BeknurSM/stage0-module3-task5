0ff01b879a03705d295abd78e387dfd325869ea5
BekoSm <b.smagulov.1999@gmail.com>
2022-07-12 18:21:04 +0600

    Push#1

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds  = minutes * 60;
+        //int minutes = 1;
+        System.out.println(seconds);
     }
 }
